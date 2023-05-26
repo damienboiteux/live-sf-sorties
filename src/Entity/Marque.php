7c63d11d2498e49d37973c1fb113dc04c902157d
Damien Boiteux <contact@damienboiteux.com>
2023-05-26 10:46:02 +0200

    migrations

--- src/Entity/Marque.php
+++ src/Entity/Marque.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\MarqueRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MarqueRepository::class)]
@@ -15,6 +17,14 @@ class Marque
 
     #[ORM\Column(length: 32)]
     private ?string $libelle = null;
+
+    #[ORM\OneToMany(mappedBy: 'marque', targetEntity: Modele::class)]
+    private Collection $modeles;
+
+    public function __construct()
+    {
+        $this->modeles = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +39,36 @@ class Marque
     public function setLibelle(string $libelle): self
     {
         $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Modele>
+     */
+    public function getModeles(): Collection
+    {
+        return $this->modeles;
+    }
+
+    public function addModele(Modele $modele): self
+    {
+        if (!$this->modeles->contains($modele)) {
+            $this->modeles->add($modele);
+            $modele->setMarque($this);
+        }
+
+        return $this;
+    }
+
+    public function removeModele(Modele $modele): self
+    {
+        if ($this->modeles->removeElement($modele)) {
+            // set the owning side to null (unless already changed)
+            if ($modele->getMarque() === $this) {
+                $modele->setMarque(null);
+            }
+        }
 
         return $this;
     }
